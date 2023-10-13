5621bf6894f2a23bbd42989c0a1e7e5838ebb30e
Jordan Pettyjohn <pettyjohnwork@gmail.com>
2023-10-13 17:08:39 -0400

    Allowed movie api data to be used

---
+++ src/components/MovieList.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+const MovieList = () => {
+    const [movies, setMovies] = useState([]);
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await fetch('https://api.themoviedb.org/3/movie/top_rated', {
+            headers: {
+              'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZDQxNjMzYjhmZGFlZTg0YWNkNTU1ZWY5MTFmZjIxMSIsInN1YiI6IjY1Mjk5YWU3MWYzZTYwMDBhYzRkODExZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.eClhzdu5OPTFJvvTOKpoetCAbfc2vVRQA2cNniX3m00', // Replace with your API key
+              'Content-Type': 'application/json',
+            },
+          });
+    
+          if (response.ok) {
+            const result = await response.json();
+            let arr = result.results;
+            setMovies(arr);
+          } else {
+            console.error('Error fetching data:', response.statusText);
+          }
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      };
+      fetchData();
+    }, []);
+  
+    return (
+      <div className="movie-list-main">
+        <div>hello</div>
+        <h2>Movie List</h2>
+        {movies.map(movie => (
+            <li key={movie.id}>
+                <Link to={{ pathname: '/movies/' + movie.id, state: { data: { id : movie.id, title: movie.title }}}}>{movie.title}</Link>
+            </li>
+          ))}
+      </div>
+    );
+};
+
+export default MovieList;
