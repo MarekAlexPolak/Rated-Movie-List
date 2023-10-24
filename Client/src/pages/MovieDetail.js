aa8ff84298aa697b5bac846088691495ff609870
Jordan Pettyjohn <pettyjohnwork@gmail.com>
2023-10-24 12:55:24 -0400

    Home, Detail,  Signup

--- Client/src/pages/MovieDetail.js
+++ Client/src/pages/MovieDetail.js
@@ -1,54 +1,96 @@
-import React, { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import { Button } from 'react-bootstrap';
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { Form, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 const MovieDetail = () => {
-    let { state } = useLocation();
-    const [movie, setMovie] = useState(null);
-    //console.log(state);
-    
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const response = await fetch('https://api.themoviedb.org/3/movie/'+state.id, {
+  let { state } = useLocation();
+  const [movie, setMovie] = useState(null);
+  //console.log(state);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://api.themoviedb.org/3/movie/" + state.id,
+          {
             headers: {
-              'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZDQxNjMzYjhmZGFlZTg0YWNkNTU1ZWY5MTFmZjIxMSIsInN1YiI6IjY1Mjk5YWU3MWYzZTYwMDBhYzRkODExZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.eClhzdu5OPTFJvvTOKpoetCAbfc2vVRQA2cNniX3m00', // Replace with your API key
-              'Content-Type': 'application/json',
+              Authorization:
+                "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZDQxNjMzYjhmZGFlZTg0YWNkNTU1ZWY5MTFmZjIxMSIsInN1YiI6IjY1Mjk5YWU3MWYzZTYwMDBhYzRkODExZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.eClhzdu5OPTFJvvTOKpoetCAbfc2vVRQA2cNniX3m00", // Replace with your API key
+              "Content-Type": "application/json",
             },
-          });
-          if (response.ok) {
-            const result = await response.json();
-            setMovie(result);
-            //console.log(result);
-          } else {
-            console.error('Error fetching data:', response.statusText);
           }
-        } catch (error) {
-          console.error('Error fetching data:', error);
+        );
+        if (response.ok) {
+          const result = await response.json();
+          setMovie(result);
+          //console.log(result);
+        } else {
+          console.error("Error fetching data:", response.statusText);
         }
-      };
-      
-      fetchData();
-    }, [window.location.pathname]);
-  
-    if (!movie) {
-      return <div>Loading...</div>;
-    }
-  
-    return (
-      <div className='movie-detail-main'>
-        <div className='movie-detail-container' >
-          <h2>{movie.title}</h2>
-          <p>{movie.production_companies[0].name}</p>
-          <p>{movie.overview}</p>
-          <p>{"Budget: " + movie.budget + " Revenue: " + movie.revenue}</p>
-          <p>{movie.release_date}</p>
-          <Button>
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, [window.location.pathname]);
+
+  const [rating, setRating] = useState("");
+  const navigate = useNavigate();
+
+  const handleUserRating = async (event) => {
+    event.preventDefault();
+    console.log(movie.id);
+    // const ratingInput = rating;
+
+    // try {
+    //   const { data } = await addRating({
+    //     variables: {
+    //       ratingMovieInput: {
+    //         movie_id: movie.id,
+    //         rating: ratingInput,
+    //       },
+    //     },
+    //   });
+    //   //Auth.login(data.addUser.token);
+
+    //   // Redirect to the home page after successful signup
+    //   navigate("/"); // Use navigate to redirect
+    // } catch (err) {
+    //   console.error(err);
+    //   // add signup error message later
+    // }
+  };
+
+  if (!movie) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="movie-detail-main">
+      <div className="movie-detail-container">
+        <h2>{movie.title}</h2>
+        <p>{movie.production_companies[0].name}</p>
+        <p>{movie.overview}</p>
+        <p>{"Budget: " + movie.budget + " Revenue: " + movie.revenue}</p>
+        <p>{movie.release_date}</p>
+        <Form onSubmit={handleUserRating}>
+          <Form.Group controlId="username">
+            <Form.Control
+              type="text"
+              placeholder="Rate"
+              value={rating}
+              onChange={(event) => setRating(event.target.value)}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
             Rate
           </Button>
-        </div>
+        </Form>
       </div>
-    );
+    </div>
+  );
 };
 
 export default MovieDetail;
--- Client/src/pages/Signup.js
+++ Client/src/pages/Signup.js
@@ -1,85 +1,89 @@
 import React, { useState } from "react";
-import { Form, Button} from "react-bootstrap";
-import { NavLink } from 'react-router-dom';
-import { useNavigate } from "react-router-dom"; 
-
+import { Form, Button } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  //const [addUser] = useMutation(ADD_USER);
+  const navigate = useNavigate();
 
-        const [username, setUsername] = useState("");
-        const [email, setEmail] = useState("");
-        const [password, setPassword] = useState("");
-        //const [addUser] = useMutation(ADD_USER);
-        const navigate = useNavigate(); 
+  const handleUserSignup = async (event) => {
+    event.preventDefault();
+    const usernameInput = username;
+    const emailInput = email;
+    const passwordInput = password;
 
+    try {
+      const { data } = await addUser({
+        variables: {
+          userInput: {
+            username: usernameInput,
+            email: emailInput,
+            password: passwordInput,
+          },
+        },
+      });
+      //Auth.login(data.addUser.token);
 
-        const handleUserSignup = async (event) => {
-            event.preventDefault();
-            const usernameInput = username;
-            const emailInput = email;
-            const passwordInput = password;
+      // Redirect to the home page after successful signup
+      navigate("/"); // Use navigate to redirect
+    } catch (err) {
+      console.error(err);
+      // add signup error message later
+    }
+  };
 
-                try {
-                    const { data } = await addUser({
-                    variables: {
-                        userInput: {
-                        username: usernameInput,
-                        email: emailInput,
-                        password: passwordInput,
-                        },
-                    },
-                    });
-                    //Auth.login(data.addUser.token);
-
-                    // Redirect to the home page after successful signup
-                    navigate("/"); // Use navigate to redirect
-                } catch (err) {
-                    console.error(err);
-                    // add signup error message later
-                }
-        };
-    return (
-        <div className="signup-main">
-            <div className="signup-container">
-                <h2>Sign Up</h2>
-                <Form onSubmit={handleUserSignup}>
-                    <Form.Group controlId="username">
-                    <Form.Control
-                        type="text"
-                        placeholder="Username"
-                        value={username}
-                        onChange={(event) => setUsername(event.target.value)}
-                    />
-                    </Form.Group>
-                    <Form.Group controlId="email">
-                    <Form.Control
-                        type="email"
-                        placeholder="Email"
-                        value={email}
-                        onChange={(event) => setEmail(event.target.value)}
-                    />
-                    </Form.Group>
-                    <Form.Group controlId="password">
-                    <Form.Control
-                        type="password"
-                        placeholder="Password"
-                        value={password}
-                        onChange={(event) => setPassword(event.target.value)}
-                    />
-                    </Form.Group>
-                </Form>
-                <div className="signup-login-prompt">
-                        <span>Already have an account?&nbsp;&nbsp;</span>
-                        <NavLink to="/login" className="inactive signup-navlink" activeClassName="active">Login</NavLink>
-                </div>
-                <div className="signup-button">
-                    <Button variant="primary" type="submit">
-                        Create Account
-                    </Button>
-                </div>
-            </div>
+  return (
+    <div className="signup-main">
+      <div className="signup-container">
+        <h2>Sign Up</h2>
+        <Form onSubmit={handleUserSignup}>
+          <Form.Group controlId="username">
+            <Form.Control
+              type="text"
+              placeholder="Username"
+              value={username}
+              onChange={(event) => setUsername(event.target.value)}
+            />
+          </Form.Group>
+          <Form.Group controlId="email">
+            <Form.Control
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+          </Form.Group>
+          <Form.Group controlId="password">
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </Form.Group>
+        </Form>
+        <div className="signup-login-prompt">
+          <span>Already have an account?&nbsp;&nbsp;</span>
+          <NavLink
+            to="/login"
+            className="inactive signup-navlink"
+            activeClassName="active"
+          >
+            Login
+          </NavLink>
         </div>
-    );
-}
+        <div className="signup-button">
+          <Button variant="primary" type="submit">
+            Create Account
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Signup;
