f0656da45eca0867ef64cbcfa1f2218b78ae6f40
Marek Polak <markalexpolak@gmail.com>
2023-10-26 18:45:50 -0400

    pain

---
+++ Server/src/main/java/com/movies/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.movies.repositories;
+
+import com.movies.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
+
+    // You can add additional custom query methods here if needed
+}
